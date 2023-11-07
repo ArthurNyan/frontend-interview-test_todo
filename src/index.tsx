e826c032007e207979db40d653ad2903e280b289
Нахатакян Артур Романович <artoim333@gmail.com>
2023-11-07 12:30:28 +0300

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,6 @@ import { Provider } from "react-redux";
 import { store } from "./app/store/store";
 import App from "./app/App";
 import reportWebVitals from './reportWebVitals';
-import "./index.css";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
